89d1f70ecebf2432565d7465f05105a8a2c850e6
Camille Rullan <>
2024-12-19 16:21:13 -0500

    server;

--- server.py
+++ server.py
@@ -4,11 +4,18 @@ from datetime import datetime
 from messages import mailbox
 import whisper as w
 import os
+import logging 
 
 # Create a data directory for your JSON file
 DATA_DIR = 'public/data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
+
+logging.basicConfig(
+    filename='server.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 model = w.load_model("tiny", download_root="/whisperdata")
 app = Flask(__name__)
@@ -27,19 +34,27 @@ def serve_files(path):
 
 @app.route('/messages', methods=['POST'])
 def new_message():
-    print("Received POST request for new message") 
+    logging.info("Received POST request for new message")
     file_size = request.content_length
-    if file_size > 10 * 1024 * 1024: # 10MB limit
+    logging.info(f"File size: {file_size}")
+    
+    if file_size > 10 * 1024 * 1024:
+        logging.error("File too large")
         return "File too large", 400
+        
     temp_file = "temp_message.mp3"
+    logging.info("Saving audio file...")
     request.files['audio'].save(temp_file)
+    
+    logging.info("Transcribing audio...")
     result = model.transcribe(temp_file, fp16=False)
     timestamp = datetime.now().strftime("%d %b %Y, %H:%M")
     os.remove(temp_file)
+    
     message = {"text": result["text"], "timestamp": timestamp}
+    logging.info(f"Message to save: {message}")
     my_mailbox.add_message(message)
-    current_messages = my_mailbox.get_messages()
-    print("Current messages in mailbox:", current_messages)
+    logging.info("Message saved")
     return "success!", 200
 
 @app.route('/messages')
