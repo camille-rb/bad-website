07db4302faa33ed251c1d711b52167484dd78976
Camille Rullan <>
2025-01-01 19:47:35 -0400

    server;

--- server.py
+++ server.py
@@ -71,5 +71,4 @@ def restore_messages():
         return f'Error restoring backup: {str(e)}', 500
 
 if __name__ == '__main__':
-    #app.run(host="0.0.0.0", port=8080, debug=True)
-    app.run(port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
